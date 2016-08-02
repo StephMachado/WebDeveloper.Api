59115f2c1e930cda2c526c7495fec70f07ec912b
StephMachado <steph_machado@hotmail.com>
2016-08-02 11:16:14 -0500

    Add project files.

---
+++ WebDeveloper.Api/Api/PersonController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebDeveloper.Api.Api
+{
+    [RoutePrefix("person")]
+    public class PersonController : ApiController
+    {
+        //Estos controladores ya no devuelven vistas si no json.
+        [HttpGet]
+        public IHttpActionResult TestGet()
+        {
+            return Ok("Ok get");
+        }
+
+        [HttpPost]
+        public IHttpActionResult TestPost()
+        {
+            return Ok("Ok post");
+        }
+
+        [HttpPut]
+        public IHttpActionResult TestPut()
+        {
+            return Ok("Ok put");
+        }
+
+        [HttpDelete]
+        public IHttpActionResult TestDelete()
+        {
+            return Ok("Ok delete");
+        }
+    }
+
+}
