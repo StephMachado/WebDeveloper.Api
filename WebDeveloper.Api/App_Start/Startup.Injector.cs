d10761d7719e1f6e4a66d7c440cd636c62bddea9
StephMachado <steph_machado@hotmail.com>
2016-08-02 18:42:44 -0500

    Agregando Startup.Injector.cs - 2016/08/02

---
+++ WebDeveloper.Api/App_Start/Startup.Injector.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using LightInject;
+
+namespace WebDeveloper.Api.App_Start
+{
+    public class Startup
+    {
+        public void ConfigInjector()
+        {
+            var container = new ServiceContainer();
+            container.RegisterAssembly(Assembly.GetExecutingAssembly());
+            container.RegisterAssembly("WebDeveloper.DataAccess*.dll");
+            container.RegisterAssembly("WebDeveloper.Model*.dll");
+            container.RegisterControllers();
+            container.EnableMvc();
+        }
+    }
+}
